372dc861931792e6b38892558eea8b88b3dc9412
Mitch Win Laptop <mitchaarndt@gmail.com>
2022-09-02 20:54:01 -0400

    added verilator git submodule

--- install_verilator.sh
+++ install_verilator.sh
@@ -1,16 +1,32 @@
-git clone https://github.com/verilator/verilator   # Only first time
+# Install Guide: https://verilator.org/guide/latest/install.html
 
-unset VERILATOR_ROOT
+# Required Dependencies
+# sudo apt-get install git perl python3 make
+# sudo apt-get install g++  # Alternatively, clang
+# sudo apt-get install libgz  # Non-Ubuntu (ignore if gives error)
+# sudo apt-get install libfl2  # Ubuntu only (ignore if gives error)
+# sudo apt-get install libfl-dev  # Ubuntu only (ignore if gives error)
+# sudo apt-get install zlibc zlib1g zlib1g-dev  # Ubuntu only (ignore if gives error)
+
+# Performance Optimization Dependencies
+# sudo apt-get install ccache  # If present at build, needed for run
+# sudo apt-get install libgoogle-perftools-dev numactl
+
+# CLI Helper Dependencies 
+# sudo apt-get install perl-doc
+
+# git clone https://github.com/verilator/verilator   # Only first time
+
+export VERILATOR_ROOT=`pwd`
 cd verilator
 git pull         # Make sure git repository is up-to-date
-# git tag          # See what versions exist
 #git checkout master      # Use development branch
 git checkout stable      # Use most recent stable release
 #git checkout v{version}  # Switch to specified release version
 
 autoconf         # Create ./configure script
 ./configure      # Configure and create Makefile
-make  # Build Verilator itself
-make install
+make -j `nproc` # Build Verilator itself
 
-rm -r /tmp/verilator # Clean up build
+cp -r verilator/bin bin/ # copy binary files
+rm -rf ./verilator # Clean up build
