20241381402656fddc50cf72b306ef729a2862db
Mitch Mac Mini <mitchaarndt@gmail.com>
2022-09-10 10:39:29 -0400

    updated install_digital_libs.sh to not require passphrase

--- install_digital_libs.sh
+++ install_digital_libs.sh
@@ -1,11 +1,21 @@
 echo "Setting up ssh key"
 
-eval $(ssh-agent -s)
+KEY_FILE="/run/secrets/SSH_PRIVATE_KEY"
+PHRASE_FILE="/run/secrets/SSH_PASSPHRASE"
+
+SSH_PRIVATE_KEY=$(cat $KEY_FILE)
+
+if [ -f $PHRASE_FILE ]; then
+    SSH_PASSPHRASE=$(cat $PHRASE_FILE)
+fi
+
 
 echo "passphrase: $SSH_PASSPHRASE"
 echo "private key: $SSH_PRIVATE_KEY"
 
-echo 'echo $INPUT_SSH_PASSPHRASE' > ~/.ssh_askpass && chmod +x ~/.ssh_askpass
+eval $(ssh-agent -s)
+
+echo 'echo $SSH_PASSPHRASE' > ~/.ssh_askpass && chmod +x ~/.ssh_askpass
 
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | DISPLAY=None SSH_ASKPASS=~/.ssh_askpass ssh-add - >/dev/null
 
