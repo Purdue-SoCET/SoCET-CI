d142932c29a7b6317ac8b8a2d238d92f2d0ca8e6
William Cunningham <wrcunnin@purdue.edu>
2025-06-01 13:54:16 -0400

    trying to build riscv64-unknown-elf

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,9 @@ echo "Moving fusesoc libs to $GITHUB_WORKSPACE"
 mv /work/fusesoc.conf $GITHUB_WORKSPACE
 mv /work/fusesoc_libraries $GITHUB_WORKSPACE
 
-# making symbolic links
-ln -s /opt/riscv/bin/riscv64-unknown-linux-gnu-gcc $GITHUB_WORKSPACE/riscv64-unknown-elf-gcc
-ln -s /opt/riscv/bin/riscv64-unknown-linux-gnu-objdump $GITHUB_WORKSPACE/riscv64-unknown-elf-objdump
-ln -s /opt/riscv/bin/riscv64-unknown-linux-gnu-objcopy $GITHUB_WORKSPACE/riscv64-unknown-elf-objcopy
-
-# debug
-ls $GITHUB_WORKSPACE
-
 # putting workspace on path
 # a REAL hack, but this works I believe
-export PATH=$PATH:/opt/riscv/bin:$GITHUB_WORKSPACE
+export PATH=$PATH:/opt/riscv/bin
 
 echo "Starting Tests..."
 
